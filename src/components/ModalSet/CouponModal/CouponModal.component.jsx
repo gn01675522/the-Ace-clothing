57198d369e1a0be058fe2a5ed9d0285a9ab33195
Wallace Lee <gn01675522@gmail.com>
2023-05-21 23:03:03 +0800

    complete CouponModal's style

--- src/components/ModalSet/CouponModal/CouponModal.component.jsx
+++ src/components/ModalSet/CouponModal/CouponModal.component.jsx
@@ -4,6 +4,8 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import "./CouponModal.styles.scss";
+
 import { selectAdminCouponsTempData } from "../../../store/adminCoupons/adminCoupons.selector";
 import { setAdminCouponsOpen } from "../../../store/adminCoupons/adminCoupons.actions";
 
@@ -11,6 +13,8 @@ import {
   createAdminCouponAsync,
   updateAdminCouponAsync,
 } from "../../../store/adminCoupons/adminCoupons.actions";
+
+import { formContent } from "./formContent.data";
 
 const defaultFormData = {
   title: "",
@@ -25,6 +29,8 @@ const CouponModal = ({ createOrEdit }) => {
   const [date, setDate] = useState(new Date());
   const dispatch = useDispatch();
   const tempCoupon = useSelector(selectAdminCouponsTempData);
+
+  console.log("inside couponmodal", tempCoupon);
 
   useEffect(() => {
     switch (createOrEdit) {
@@ -66,117 +72,101 @@ const CouponModal = ({ createOrEdit }) => {
 
   return (
     <>
-      <div className="modal-dialog modal-lg">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="exampleModalLabel">
-              {createOrEdit === "create"
-                ? "建立新優惠券"
-                : `編輯 ${formData.title}`}
-            </h1>
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-              onClick={onCloseModalHandler}
-            />
-          </div>
-          <div className="modal-body">
-            <div className="mb-2">
-              <label className="w-100" htmlFor="title">
-                標題
-                <input
-                  type="text"
-                  id="title"
-                  placeholder="請輸入標題"
-                  name="title"
-                  className="form-control mt-1"
-                  value={formData.title}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <div className="row">
-              <div className="col-md-6 mb-2">
-                <label className="w-100" htmlFor="percent">
-                  折扣（%）
-                  <input
-                    type="text"
-                    name="percent"
-                    id="percent"
-                    placeholder="請輸入折扣（%）"
-                    className="form-control mt-1"
-                    value={formData.percent}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="col-md-6 mb-2">
-                <label className="w-100" htmlFor="due_date">
-                  到期日
-                  <input
-                    type="date"
-                    id="due_date"
-                    name="due_date"
-                    placeholder="請輸入到期日"
-                    className="form-control mt-1"
-                    value={`${date.getFullYear().toString()}-${(
-                      date.getMonth() + 1
-                    )
-                      .toString()
-                      .padStart(2, 0)}-${date
-                      .getDate()
-                      .toString()
-                      .padStart(2, 0)}`}
-                    onChange={(e) => {
-                      setDate(new Date(e.target.value));
-                    }}
-                  />
-                </label>
-              </div>
-              <div className="col-md-6 mb-2">
-                <label className="w-100" htmlFor="code">
-                  優惠碼
-                  <input
-                    type="text"
-                    id="code"
-                    name="code"
-                    placeholder="請輸入優惠碼"
-                    className="form-control mt-1"
-                    value={formData.code}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-            </div>
-            <label className="form-check-label" htmlFor="is_enabled">
-              <input
-                className="form-check-input me-2"
-                type="checkbox"
-                id="is_enabled"
-                name="is_enabled"
-                checked={!!formData.is_enabled}
-                onChange={handleChange}
-              />
+      <div className="coupon-modal">
+        <div className="coupon-modal__header">
+          <h1 className="coupon-modal__header-title">
+            {createOrEdit === "create"
+              ? "建立新優惠券"
+              : `優惠券名稱： ${formData.title}`}
+          </h1>
+          <button
+            type="button"
+            className="coupon-modal__header-button"
+            aria-label="Close"
+            onClick={onCloseModalHandler}
+          >
+            X
+          </button>
+        </div>
+
+        <div className="coupon-modal__body">
+          <div className="coupon-modal__body-check">
+            <label
+              className="coupon-modal__body-check-label"
+              htmlFor="is_enabled"
+            >
               是否啟用
             </label>
+            <input
+              className="coupon-modal__body-check-input"
+              type="checkbox"
+              id="is_enabled"
+              name="is_enabled"
+              checked={!!formData.is_enabled}
+              onChange={handleChange}
+            />
           </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={onCloseModalHandler}
-            >
-              關閉
-            </button>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={onSubmitHandler}
-            >
-              儲存
-            </button>
+          <div className="coupon-modal__body-content">
+            {formContent.map((content) => {
+              return (
+                <div className="coupon-modal__body-content-group">
+                  <label
+                    className="coupon-modal__body-content-group-label"
+                    htmlFor={content.id}
+                  >
+                    {content.title}
+                  </label>
+                  <input
+                    type={content.type}
+                    id={content.id}
+                    placeholder={
+                      content.id === "percent"
+                        ? undefined
+                        : `請輸入${content.title}`
+                    }
+                    name={content.id}
+                    className="coupon-modal__body-content-group-input"
+                    value={
+                      content.id === "due_date"
+                        ? `${date.getFullYear().toString()}-${(
+                            date.getMonth() + 1
+                          )
+                            .toString()
+                            .padStart(2, 0)}-${date
+                            .getDate()
+                            .toString()
+                            .padStart(2, 0)}`
+                        : formData[content.id]
+                    }
+                    onChange={
+                      content.id === "due_date"
+                        ? (e) => {
+                            setDate(new Date(e.target.value));
+                          }
+                        : handleChange
+                    }
+                  />
+                </div>
+              );
+            })}
           </div>
+        </div>
+
+        <div className="coupon-modal__footer">
+          <button
+            type="button"
+            className="coupon-modal__footer-close"
+            onClick={onCloseModalHandler}
+          >
+            關閉
+          </button>
+          <button
+            type="button"
+            className="coupon-modal__footer-save"
+            onClick={onSubmitHandler}
+          >
+            儲存
+          </button>
         </div>
       </div>
     </>
---
+++ src/components/ModalSet/CouponModal/formContent.data.js
@@ -0,0 +1,6 @@
+export const formContent = [
+  { id: "title", type: "text", title: "標題" },
+  { id: "code", type: "text", title: "優惠碼" },
+  { id: "percent", type: "number", title: "折扣(%)" },
+  { id: "due_date", type: "date", title: "到期日" },
+];
