cdec8c6cc2c75a2bdc03811125ec7146db6c2a9a
gn01675522 <gn01675522@gmail.com>
2023-08-08 20:15:51 +0800

    refactor: refactor structure on Home and Navbar component

--- src/pages/front/Home/Home.component.jsx
+++ src/pages/front/Home/Home.component.jsx
@@ -56,12 +56,14 @@ const Home = () => {
       <div className="home" ref={scrollDetectParentRef}>
         <Banner />
         <div className="home__discount">
-          <h3>Grand Opening Sale</h3>
-          <h2>EXTRA 30% OFF SALE</h2>
-          <p>
-            慶祝您我的相遇，也歡迎您加入我們的旅程，輸入 newBeginning
-            即可享全品項 7 折優惠！
-          </p>
+          <div className="home__discount-content">
+            <h3>Grand Opening Sale</h3>
+            <h2>EXTRA 30% OFF SALE</h2>
+            <p>
+              慶祝您我的相遇，也歡迎您加入我們的旅程，輸入 newBeginning
+              即可享全品項 7 折優惠！
+            </p>
+          </div>
         </div>
         <div className="home__session">
           <h2 className="home__session-title">SHOP NOW</h2>
