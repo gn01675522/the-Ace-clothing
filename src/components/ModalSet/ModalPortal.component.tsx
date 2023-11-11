e62c3c0ee0320c20bf6f94e6042ce0f86eb48647
gn01675522 <gn01675522@gmail.com>
2023-11-11 22:14:19 +0800

    ts-migration: ModalProtal

--- src/components/ModalSet/ModalPortal.component.tsx
+++ src/components/ModalSet/ModalPortal.component.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import { createPortal } from "react-dom";
 
 import ModalBackdrop from "./ModalBackdrop/ModalBackdrop.component";
@@ -7,17 +8,18 @@ import CouponModal from "./CouponModal/CouponModal.component";
 import OrderModal from "./OrderModal/OrderModal.component";
 import DeleteInCartModal from "./DeleteInCartModal/DeleteInCartModal.component";
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById("overlays") as HTMLElement;
+// 此為永存於 index.html 的一個結構，專門用來做 modal portal 用。
 
-export const MODAL_TYPE = {
-  product: "product",
-  coupon: "coupon",
-  order: "order",
-  delete: "delete",
-  deleteInCart: "deleteInCart",
-};
+export enum MODAL_TYPE {
+  product = "product",
+  coupon = "coupon",
+  order = "order",
+  delete = "delete",
+  deleteInCart = "deleteInCart",
+}
 
-const modalSwitch = (modalType) =>
+const modalSwitch = (modalType: MODAL_TYPE): React.ComponentType<any> =>
   ({
     [MODAL_TYPE.product]: ProductModal,
     [MODAL_TYPE.coupon]: CouponModal,
@@ -25,8 +27,18 @@ const modalSwitch = (modalType) =>
     [MODAL_TYPE.delete]: DeleteModal,
     [MODAL_TYPE.deleteInCart]: DeleteInCartModal,
   }[modalType]);
+// 透過 mapping 方式來 return 相對應的 component
 
-const ModalPortal = ({ openWhichModal, backdropClose, ...otherProps }) => {
+type ModalPortalProps = {
+  openWhichModal: MODAL_TYPE;
+  backdropClose: string;
+};
+
+const ModalPortal: FC<ModalPortalProps> = ({
+  openWhichModal,
+  backdropClose,
+  ...otherProps
+}) => {
   const Modal = modalSwitch(openWhichModal);
 
   return (
